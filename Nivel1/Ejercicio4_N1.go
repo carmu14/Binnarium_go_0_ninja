072f585ee3b4fcb26845849e5c219c76ba57d229
carmu14 <carmu14@gmail.com>
2021-11-21 18:57:03 -0500

    Ejercicios nivel 1

--- Nivel1/Ejercicio4_N1.go
+++ Nivel1/Ejercicio4_N1.go
@@ -1,1 +1,14 @@
 package main
+
+import "fmt"
+
+type num int
+var a num
+
+func main() {
+	fmt.Println(a)
+	fmt.Printf("El tipo de x es: %T\n", a)
+
+	a = 42
+	fmt.Println(a)
+}
--- Nivel1/Ejercicio5_N1.go
+++ Nivel1/Ejercicio5_N1.go
@@ -1,1 +1,19 @@
 package main
+
+import "fmt"
+
+type num int
+var a num
+var b int
+
+func main() {
+	fmt.Println(a)
+	fmt.Printf("El tipo de x es: %T\n", a)
+
+	a = 42
+	fmt.Println(a)
+
+	b = int(a)
+	fmt.Println(b)
+	fmt.Printf("%T", b)
+}
