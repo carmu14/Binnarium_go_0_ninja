71d3e07701426c252cb349f37b9f4d8e5bf39c56
carmu14 <carmu14@gmail.com>
2021-11-23 11:10:32 -0500

    modificarTEST
    Se modifica el archivo main del ejercicio 5 del N11 para obtener un error en el test

--- Nivel11/Ejercicio5_N11/main.go
+++ Nivel11/Ejercicio5_N11/main.go
@@ -1,14 +1,17 @@
 //Paquete para entender la suma de dos numeros
 package main
 
+/*
+	Nos permite sumar dos numeros enteros
+*/
 import "fmt"
 
 func main() {
 	result := sumar(5, 2)
-	fmt.Printf("El resultado de la suma es: %v", result)
+	fmt.Printf("El resultado de la suma es: %d", result)
 
 }
 
 func sumar(op1 int, op2 int) int {
-	return op1 + op2
+	return op1 - op2
 }
