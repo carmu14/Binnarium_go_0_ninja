8f3dc38ead9cf5498ce4f02ec2026f16b122d25a
carmu14 <carmu14@gmail.com>
2021-11-23 10:29:04 -0500

    cofiguraionMOD

--- Nivel11/Ejercicio5_N11/main_test.go
+++ Nivel11/Ejercicio5_N11/main_test.go
@@ -1,12 +1,16 @@
 package main_test
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestSumar(t *testing.T) { // Recibir struct de tipo testing.T
 	resultado := sumar(5, 2)
 	resultadoEsperado := 7
 	if resultado != resultadoEsperado {
 		t.Errorf("Error en %s. Se esperaba %d pero el resultado fue %d", t.Name(), resultadoEsperado, resultado)
+	} else {
+		fmt.Println("El resultado es correcto")
 	}
-	// Y si no, o sea, si todo va bien, no hacemos nada
 }
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/carmu14/go_binnarium
+
+go 1.17
